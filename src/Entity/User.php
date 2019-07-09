8186c87e5269e9815f463547234fd84e7312d644
FingerFRK <shayempvp@gmail.com>
2019-07-09 16:46:45 +0200

    Changements BDD & EasyAdmin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,12 +2,14 @@
 
     namespace App\Entity;
 
+    use Symfony\Component\Validator\Constraints as Assert;
     use FOS\UserBundle\Model\User as BaseUser;
     use Doctrine\ORM\Mapping as ORM;
 
     /**
      * @ORM\Entity
-     * @ORM\Table(name="fos_user")
+     * @ORM\Table(name="`user`")
+     * @ORM\HasLifecycleCallbacks
      */
     class User extends BaseUser
     {
@@ -22,9 +24,135 @@
          * @ORM\Column(type="string", length=32)
          * @Assert\NotBlank
          */
+        private $prenom;
 
+        /**
+         * @ORM\Column(type="string", length=36)
+         * @Assert\NotBlank
+         */
+        private $nom;
+
+        /**
+         * @ORM\Column(type="string", length=32)
+         * @Assert\NotBlank
+         */
+        private $adresse;
+
+        /**
+         * @ORM\Column(type="string", length=32)
+         * @Assert\NotBlank
+         */
+        private $ville;
+
+        /**
+         * @ORM\Column(type="string", length=5)
+         * @Assert\NotBlank
+         * @Assert\Length(max="5", min="5", minMessage="Code postal invalide", maxMessage="Code postal invalide")
+         * @Assert\Regex("/[0-9]{5,5}/", message="Code postal invalide")
+         */
+        private $code_postal;
+
+        /**
+         * @ORM\Column(type="string", length=10)
+         * @Assert\NotBlank
+         * @Assert\Length(max="10", min="10", minMessage="Téléphone invalide", maxMessage="Téléphone invalide")
+         * @Assert\Regex("/[0]{1,1}[0-9]{9,9}/", message="Téléphone invalide")
+         */
+        private $telephone;
+
+        /**
+         * @ORM\Column(type="datetime")
+         */
+        private $created_at;
+
+
+        public function getPrenom(): ?string
+        {
+            return $this->prenom;
+        }
+    
+        public function setPrenom(string $prenom): self
+        {
+            $this->prenom = $prenom;
+    
+            return $this;
+        }
+    
+        public function getNom(): ?string
+        {
+            return $this->nom;
+        }
+    
+        public function setNom(string $nom): self
+        {
+            $this->nom = $nom;
+    
+            return $this;
+        }
+    
+        public function getAdresse(): ?string
+        {
+            return $this->adresse;
+        }
+    
+        public function setAdresse(string $adresse): self
+        {
+            $this->adresse = $adresse;
+    
+            return $this;
+        }
+    
+        public function getVille(): ?string
+        {
+            return $this->ville;
+        }
+    
+        public function setVille(string $ville): self
+        {
+            $this->ville = $ville;
+    
+            return $this;
+        }
+    
+        public function getCodePostal(): ?string
+        {
+            return $this->code_postal;
+        }
+    
+        public function setCodePostal(string $code_postal): self
+        {
+            $this->code_postal = $code_postal;
+    
+            return $this;
+        }
+    
+        public function getTelephone(): ?string
+        {
+            return $this->telephone;
+        }
+    
+        public function setTelephone(string $telephone): self
+        {
+            $this->telephone = $telephone;
+    
+            return $this;
+        }
+
+        public function getCreatedAt(): ?\DateTimeInterface
+        {
+            return $this->created_at;
+        }
+
+        public function setCreatedAt()
+        {
+            
+            return $this;
+        }
+        
         public function __construct()
         {
             parent::__construct();
+            $this->created_at = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
+            
         }
     }
---
+++ src/Migrations/Version20190709124038.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190709124038 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user ADD prenom VARCHAR(32) NOT NULL, ADD nom VARCHAR(36) NOT NULL, ADD adresse VARCHAR(32) NOT NULL, ADD ville VARCHAR(32) NOT NULL, ADD code_postal VARCHAR(5) NOT NULL, ADD telephone VARCHAR(10) NOT NULL, ADD created_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user DROP prenom, DROP nom, DROP adresse, DROP ville, DROP code_postal, DROP telephone, DROP created_at');
+    }
+}
